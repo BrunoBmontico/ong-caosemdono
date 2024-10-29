32de8f6816b7951be9852cf00788bbea211fad3c
BrunoBmontico <aluno103.23187204bruno@gmail.com>
2024-10-28 21:59:15 -0300

    pagina quem somos finalizada

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        arrows: false
+    });
+});
