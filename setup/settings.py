ee02b97c67ef6b74e641824a7cfda5cdbae27a81
BrunoBmontico <aluno103.23187204bruno@gmail.com>
2024-11-02 13:42:13 -0300

    'arrumando_static_files'

--- setup/settings.py
+++ setup/settings.py
@@ -108,7 +108,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIR = [
     BASE_DIR / "core/static",
-    ]
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
